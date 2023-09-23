a74c347acdaea67c8b1794b3e35f1f26808be2ed
Sergio_Mir <voidtracer6@gmail.com>
2023-09-23 13:02:40 -0400

    MOD: ./vite-config.js: changed build and deployment configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  base: 'https://sergiomir018.github.io/Gamor/',
+  server: {
+    port: 8080
+  }
 })
