5bcafb2ad27381ece6fcb98e00e3549dc79931be
Sergio_Mir <voidtracer6@gmail.com>
2023-09-26 01:04:38 -0400

    MOD
    done some style changes on the main-display component, specifically on the first third of it

--- src/components/Main-Display/Main-Display.jsx
+++ src/components/Main-Display/Main-Display.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import '../../styles/Main-Display.css'
 
 export default function MainDisplay() {
   return (
-    <div>
-      
+    <div className='main-display'>
+      <div className='streaming-sing-in'>
+        <h1 id='slogan-top'>start</h1>
+        <h1 id='slogan-purple'>streaming</h1>
+        <h1 id='slogan'>games</h1>
+        <h1 id='slogan'>differently</h1>
+        <p>gamor now has <strong id='feature-highlight'>stream party</strong> platform</p>
+        <div className='links-sing-in'></div>
+        <ul className='account-links'>
+          <li><a href="#">Create account</a></li>
+          <li><a href="#">Sing in</a></li>
+        </ul>
+      </div>
+      <div className='game-display'>Game thing</div>
+      <div className='search-section'>Search thing</div>
     </div>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,17 +6,17 @@ export default function NavBar() {
     <nav className='navbar'>
       <div className='hub-sections'>
         <ul className='hub-sections-links'>
-          <li><a>Home</a></li>
-          <li><a>Streams</a></li>
-          <li><a>Party</a></li>
-          <li><a>Premium</a></li>
+          <li><a href='#'>Home</a></li>
+          <li><a href='#'>Streams</a></li>
+          <li><a href='#'>Party</a></li>
+          <li><a href='#'>Premium</a></li>
         </ul>        
       </div>
       <h1 className='hub-name'>Gamor</h1>
       <div className='profile-section'>
         <ul className='profile-section-links'>
-          <li id='sing-in'><a>Sing In</a></li>
-          <li id='create-account'><a>Create Account</a></li>
+          <li id='sing-in'><a href='#'>Sing In</a></li>
+          <li id='create-account'><a href='#'>Create Account</a></li>
         </ul>
       </div>
     </nav>
