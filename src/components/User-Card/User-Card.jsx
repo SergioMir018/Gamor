e09cccde9f63c8ccf0cd95a28027c393596a69bb
Sergio_Mir <voidtracer6@gmail.com>
2023-09-27 21:14:36 -0400

    ADD: new component for the users
    new component to represent the results of the searches for the users representation

---
+++ src/components/User-Card/User-Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import '../../styles/User-Card.css'
+
+export default function UserCard() {
+  return (
+    <div className='user-card'>
+      Hola
+    </div>
+  )
+}
