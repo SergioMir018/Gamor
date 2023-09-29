f73dc1213bbf42ca56aa1a743c4e82ebe1a943d6
Sergio_Mir <voidtracer6@gmail.com>
2023-09-28 22:16:14 -0400

    cambios varios

--- src/components/User-Pic/User-Pic.jsx
+++ src/components/User-Pic/User-Pic.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import '../../styles/User-Pic.css';
 
-import Cosa from '../../assets/avatars/avatar1.webp'
-
-
 export default function UserPic({avatar}) {
   return (
-    
     <div className='pic-container'>
       <img src={avatar} alt="User Avatar" />
     </div>
