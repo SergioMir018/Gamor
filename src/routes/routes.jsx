2b1f5f375344e60a8e83e41a2730616651d89dc5
Sergio_Mir <voidtracer6@gmail.com>
2023-09-30 05:15:24 -0400

    MOD: mod in the route creator type

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 
 import Login from '../components/Login/Login';
 import MainHub from '../components/Main/Main-Hub';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: '/Gamor/',
     element: <MainHub />
