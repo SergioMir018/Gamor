936668a3fb292fb7db3bb4e036b1a926351e72b1
Sergio_Mir <voidtracer6@gmail.com>
2023-09-30 12:13:06 -0400

    MOD: new build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: 'https://sergiomir018.github.io/Gamor/',
+  base: 'https://sergiomir018.github.io/',
   server: {
     port: 8080
   },
